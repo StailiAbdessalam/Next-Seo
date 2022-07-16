aad62ad3c1e9403d31ddb3e11b2cfdbd989b158f
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-16 18:23:31 +0100

    import default seo in app web

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
-
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
